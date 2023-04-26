4d9ccbded5434abed6671965320a1ea330f93aeb
PEIWEN-Vivian-Yu <pwy8900@uw.edu>
2023-04-26 16:51:59 -0700

    publication page update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Scholars } from './scholars'
 import { Home } from './Homepage'
+import { Publications } from './Publications'
 import { Routes, Route, Navigate, useNavigate, Outlet } from 'react-router-dom';
 
 
@@ -11,6 +12,7 @@ function App() {
         <Route>
           <Route path='/home' element={<Home />} />
           <Route path='/scholars' element={<Scholars />} />
+          <Route path='/publications' element={<Publications />} />
           <Route path='*' element={<Navigate to='/home' />} />
         </Route>
       </Routes>
---
+++ src/Publications.js
@@ -0,0 +1,37 @@
+import Navbar from "./Navbar";
+import Footer from './Footer.js'
+
+
+function generatePublications() {
+
+
+    
+
+
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+
+
+export function Publications(props) {
+
+    
+    return (
+        <main>
+            <div>
+                <Navbar />
+                <div className="filter">
+                    <p>Filter</p>
+                    
+                </div>
+            </div>
+            
+        </main>
+        
+    )
+}
