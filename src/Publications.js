dbf87446db2419d64a6617153464b3aaa5d791d3
PEIWEN-Vivian-Yu <pwy8900@uw.edu>
2023-04-26 17:28:03 -0700

    add a dropbox

--- src/Publications.js
+++ src/Publications.js
@@ -4,9 +4,7 @@ import Footer from './Footer.js'
 
 function generatePublications() {
 
-
     
-
 
 
     return (
@@ -27,6 +25,13 @@ export function Publications(props) {
                 <Navbar />
                 <div className="filter">
                     <p>Filter</p>
+                    <div className="searchFilter">
+                        <select id="department">
+                            <option value="any">Any</option>
+                            <option value="option2">Option 2</option>
+                            <option value="option3">Option 3</option>
+                        </select>
+                    </div>
                     
                 </div>
             </div>
