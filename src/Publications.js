9199dd44cab6ca427e9bdf48f0b64e2562b1eb4f
Jinwoo Kim <jinwookim9911@gmail.com>
2023-04-29 00:49:47 -0700

    publications page in progress

--- src/Publications.js
+++ src/Publications.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
-import Footer from './Footer.js'
+import Footer from './Footer.js';
+import './Publications.css';
 
 
 function generatePublications() {
@@ -20,23 +21,48 @@ export function Publications(props) {
 
     
     return (
-        <main>
-            <div>
-                <Navbar />
+        <div>
+            <Navbar />
+            <div className="publicationResult">
                 <div className="filter">
-                    <p>Filter</p>
+                    <h3>Filter</h3>
                     <div className="searchFilter">
-                        <select id="department">
+                        <input className="searchBar" type="text" placeholder="Search..."/>
+                        <p>Department</p>
+                        <select className="department">
+                            <option value="any">Any</option>
+                            <option value="option2">Option 2</option>
+                            <option value="option3">Option 3</option>
+                        </select>
+                        <p>Field of Interest</p>
+                        <select className="department">
+                            <option value="any">Any</option>
+                            <option value="option2">Option 2</option>
+                            <option value="option3">Option 3</option>
+                        </select>
+                        <p>Location</p>
+                        <select className="department">
+                            <option value="any">Any</option>
+                            <option value="option2">Option 2</option>
+                            <option value="option3">Option 3</option>
+                        </select>
+                        <p>Institution</p>
+                        <select className="department">
                             <option value="any">Any</option>
                             <option value="option2">Option 2</option>
                             <option value="option3">Option 3</option>
                         </select>
                     </div>
-                    
+                </div>
+                <div className="result">
+                    <div className="resultTitle">
+                        <h1>Publication</h1>
+                    </div>
+                    <div><p>result</p></div>
+                    <div><p>result</p></div>
                 </div>
             </div>
-            
-        </main>
-        
+            <Footer />
+        </div>   
     )
 }
