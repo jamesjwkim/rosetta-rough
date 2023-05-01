7d0e6108c2f91cac0f0764681efc2c03a888e4e8
Jinwoo Kim <jinwookim9911@gmail.com>
2023-04-30 23:23:19 -0700

    pub page updated

--- src/Publications.js
+++ src/Publications.js
@@ -85,12 +85,15 @@ export function Publications(props) {
                     </div>
                     <div className="resultPagination">
                         <a href="#">&laquo;</a>
-                        <a href="#">1</a>
+                        <a href="#" class="active">1</a>
                         <a href="#">2</a>
                         <a href="#">3</a>
                         <a href="#">4</a>
                         <a href="#">5</a>
                         <a href="#">6</a>
+                        <a href="#">7</a>
+                        <a href="#">8</a>
+                        <a href="#">9</a>
                         <a href="#">&raquo;</a>
                     </div>
                 </div>
